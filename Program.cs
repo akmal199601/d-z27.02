c281e5aa04034e25942f7247f23a302a1c02e06e
akmal199601 <akmal199601@mail.ru>
2020-03-27 19:58:04 +0300

    Added

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace aki2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Задание 1
+            /// Вариант 1
+            //   double a = 3000;
+            //   if (a > 1000)
+            //   {
+            //     a -= a*0.05;/// скидка на 5 % 
+            //   }
+            //  else if (a > 500)
+            //  {
+            //     a -= a*0.03;/// скидка на 3 %
+            //    }
+            //   System.Console.WriteLine(a);
+
+            // ///2 Вариант 
+
+            // decimal a = 3333,sale = 0;
+            // if (a >= 1000){
+            //         sale = a / 100*5;
+            // }
+            // esle if (a >=500)
+            // {
+            //     sale a / 100 * 3;
+            // }
+            // System.Console.WriteLine($"Со скидкой {a}-sale)");
+
+            ////////////Задание 3/////
+
+            // double a = 2, b = 3, c = 4, d = 5, min = a;
+            // if (a < b && b < c && c < d)
+            // {
+            //     System.Console.WriteLine("Возростание");
+            // }
+            // else if (a == b && a == c && a == d)
+            // {
+            //     System.Console.WriteLine(a * b * c * d);
+            // }
+            // else
+            // {
+            //     if (min > b)
+            //     {
+            //         min = b;
+            //     }
+            //     if (min > c)
+            //     {
+            //         min = c;
+            //     }
+            //     if (min > d)
+            //     {
+            //         min = d;
+            //     }
+            // }
+            //      Console.ReadLine();
+
+            ///Задание 4 
+
+            //  int a = 2, b = 3, c = 4, min = a, mid = b, max = c;
+            //  if (max < b)
+            //  {
+            //      max=b;
+            //  }
+            //  if(max < c) 
+            //  {
+            //      max=c;
+            //  }  
+            //  if (min>b)
+            // {
+            //     min=b;
+            // }
+            // if(min > c)
+            // {
+            //     min=c;
+            // }
+            // if(a!=max&&a!=min)
+            // {
+            //     mid = a;
+            // }
+            // if (b!=max&&b!=min)
+            // {
+            //     mid = b;
+            // }
+            // if (c!=max&&c!=min)
+            // {
+            //     mid = c;
+            // }
+            // System.Console.WriteLine($"a={a} b={b} c={c}");
+
+          
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
